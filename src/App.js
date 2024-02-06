0c09718dcac0d97c0d2403ba99ce9c1a76515118
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-02-06 18:45:28 +0800

    2/6/2024 Tu 6:45p - Created functioning Grade average calculator using useState in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useTransition } from "react";
 
-function App() {
+// function isReady() {
+//   return 
+// }
+
+function Grades() {
+  const [Quiz, setQuizScore] = useState(0);
+  const [LabActivity, setLabActivityScore] = useState(0);
+  const [FinalExam, setFinalExamScore] = useState(0);
+
+  const isReady = () => {
+    return (Quiz != 0 && LabActivity != 0 && FinalExam != 0);
+  };
+
+  const resetForm = () => {
+    setQuizScore(0);
+    setLabActivityScore(0);
+    setFinalExamScore(0);
+  };
+
+  return <div>
+    <div className="card">
+
+      <div className="card-body bg-info">
+        <h5 className="card-title">Grades Calculator</h5>
+      </div>
+
+      <div className="card-body">
+        <form>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" id="addon-wrapping">Quiz</span>
+            <input type="number" className="form-control" onChange={(e) => setQuizScore(e.target.value)} value={Quiz} id="quiz" name="quiz" />
+          </div>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" id="addon-wrapping">Lab Activity</span>
+            <input type="number" className="form-control" onChange={(e) => setLabActivityScore(e.target.value)} value={LabActivity} id="labActivity" name="labActivity" />
+          </div>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" id="addon-wrapping">Final Exam</span>
+            <input type="number" className="form-control" onChange={(e) => setFinalExamScore(e.target.value)} value={FinalExam} id="finalExam" name="finalExam" />
+          </div>
+          <button class="btn btn-danger" onClick={resetForm}><i class="fa fa-rotate-right"></i></button>
+        </form>
+      </div>
+
+      <div className="card-body border border-primary-subtle" style={{ display: isReady() ? 'initial' : 'none' }}>
+        AvgGrade: {Math.round(((parseInt(FinalExam) + parseInt(LabActivity) + parseInt(Quiz)) / 3 + Number.EPSILON) * 100) / 100}
+      </div>
+
+    </div>
+  </div>;
+}
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Grades />
       </header>
     </div>
   );
 }
-
-export default App;
